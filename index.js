66f8861b335d08f9526aefe4da9a9f5b098d1fa2
cmdn-hieutran <hieu.tran@classmethod.tech>
2025-04-23 09:46:20 +0700

    create a variable in index.js file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log('Hello, world! This is a test of branch FBI-1.')
 let conflict = 'This is a conflict test.'
 console.log(`Conflict: ${conflict}`)
+let test = 'This is a test of branch main.'
